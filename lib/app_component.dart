be80b9074480ce797f0e0faeed3f72e0af1967e7
Sandbox <sandbox@lama.hipro>
2018-09-10 19:26:43 +0530

    Add Routing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,28 +1,26 @@
 import 'package:angular/angular.dart';
-import 'src/hero.dart';
-import 'src/hero_component.dart';
+import 'package:angular_router/angular_router.dart';
+import 'src/routes.dart';
+import 'src/route_paths.dart';
 import 'src/hero_service.dart';
-import 'dart:async';
 @Component(
   selector: 'my-app',
-  templateUrl: 'app_component.html',
+  template: '''
+  	    <h1> {{title}} </h1>
+	    <nav>
+		<a [routerLink]="RoutePaths.dashboard.toUrl()"
+       		   [routerLinkActive]="'active'"> Dashboard </a>
+		<a [routerLink]="RoutePaths.heroes.toUrl()"
+		   [routerLinkActive]="'active'"> Heroes </a>
+	    </nav>
+	    <router-outlet [routes]="Routes.all"></router-outlet>
+	    ''',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, HeroComponent],
+  directives: [routerDirectives],
+  exports: [Routes, RoutePaths],
   providers: [ClassProvider(HeroService)],
-)
-class AppComponent implements OnInit{
-  final title = 'Tour of Heroes';
-  List<Hero> heroes;
-  Hero selected;
-  final HeroService _heroService;
-
-  AppComponent(this._heroService);
   
-  void onSelect(Hero hero) => selected = hero;
-
-  _getHeroes() async {
-       heroes = await _heroService.getAll();
-  }
-
-  void ngOnInit() => _getHeroes();
+)
+class AppComponent {
+  final title = 'Tour of Heroes';
 }
---
+++ lib/src/dashboard_component.dart
@@ -0,0 +1,24 @@
+import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'hero.dart';
+import 'route_paths.dart';
+import 'hero_service.dart';
+@Component(
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard_component.html',
+  styleUrls: ['dashboard_component.css'],
+  directives: [routerDirectives, coreDirectives],
+)
+class DashboardComponent implements OnInit {
+      List<Hero> heroes;
+      final HeroService _heroService;
+      
+      DashboardComponent(this._heroService);
+
+      @override
+      void ngOnInit() async {
+      	   heroes = await _heroService.getAll();
+      }
+
+      String heroUrl(int id) => RoutePaths.hero.toUrl(parameters: {idParam: '$id'});
+}
